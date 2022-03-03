11fb0a32da687abf20ab22cfdcd6a77380354fb3
Chris <christopher.douvry@gmail.com>
2022-03-03 09:49:53 -0700

    Prepping before tailwind overhaul

---
+++ src/tests.rs
@@ -0,0 +1,25 @@
+// pub mod exponential;
+// pub mod hyperbolic;
+// pub mod inputs;
+
+// use crate::exponential::Exponential;
+// use crate::hyperbolic::Hyperbolic;
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     fn solve_q_np_t() {
+//         let decline:Hyperbolic<f32> = {
+//             qi:ForecastParameter::Known(1350.0),
+//             q:ForecastParameter::Unknown,
+//             di:ForecastParameter::Known(3.5513),
+//             d:ForecastParameter::Known(0.1054),
+//             t:ForecastParameter::Unknown,
+//             np:ForecastParameter::Unknown,
+//             b:ForecastParameter::Known(0.95),
+//         };
+
+//         assert_eq!
+//     }
+// }
